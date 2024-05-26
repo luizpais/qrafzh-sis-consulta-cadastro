65f1d66a25b35c0abcd80a55296d9352af6ede01
Luiz Pais <luiz.pais@gmail.com>
2024-05-26 10:52:22 -0300

    Docker compose enablement

--- src/main/java/org/quarkusclub/repositories/ConveniosRepository.java
+++ src/main/java/org/quarkusclub/repositories/ConveniosRepository.java
@@ -3,8 +3,9 @@ package org.quarkusclub.repositories;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import lombok.Value;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.quarkusclub.dtos.ClienteStatusResponse;
 import org.quarkusclub.restclients.ConveniadosService;
 
@@ -20,13 +21,19 @@ public class ConveniosRepository {
     private static final Logger log = Logger.getLogger(ConveniosRepository.class);
     private final Map<String, ConveniadosService> convenios = new HashMap<>();
 
+    @ConfigProperty( name = "convenio.boa-vida.url")
+    private String boaVidaUrl;
+
+    @ConfigProperty( name = "convenio.vida-longa.url")
+    private String vidaLongaUrl;
+
     private ConveniosRepository() {
         convenios.put("boa vida", RestClientBuilder.newBuilder()
-                .baseUri(URI.create("http://localhost:8081/"))
+                .baseUri(URI.create(boaVidaUrl))
                 .build(ConveniadosService.class));
 
         convenios.put("vida longa", RestClientBuilder.newBuilder()
-                .baseUri(URI.create("http://localhost:8082/"))
+                .baseUri(URI.create(vidaLongaUrl))
                 .build(ConveniadosService.class));
     }
 
